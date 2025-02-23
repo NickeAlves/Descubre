54030df4cd1c34d8cd56e4480d3dca27ee78b4b4
Nicolas Alves <nicke.rodrigues1125@gmail.com>
2025-02-23 14:30:00 +0100

    :sparkles: adjust frontend files
    Signed-off-by: Nicolas Alves <nicke.rodrigues1125@gmail.com>

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="text-black">¡ Bienvenido a Descubre!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
